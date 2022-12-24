fbe00fdc02df18e39c41651135d0b70a3902b8c2
AlexPanaev <panaev.as@gmail.com>
2022-12-25 00:01:17 +0400

    my_branch_1: feature/my_branch_1 signUP from start to end with different tests

--- src/main/java/aqa/bShareLane/SignUpVersion2.java
+++ src/main/java/aqa/bShareLane/SignUpVersion2.java
@@ -20,18 +20,18 @@ public class SignUpVersion2 {
     WebDriver driver;
 
     @BeforeClass
-    public void setPathToWebDriver(){
-        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+    public void setPathToWebDriver() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
     }
 
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
         driver = new ChromeDriver();
         driver.get("https://www.sharelane.com/cgi-bin/register.py");
     }
 
     @Test
-    public void zipCodeShouldBevalid(){
+    public void zipCodeShouldBevalid() {
         WebElement zip_code_field = driver.findElement(By.name("zip_code"));
         zip_code_field.sendKeys("67890");
         driver.findElement(By.cssSelector("input[value='Continue']")).click();
@@ -41,7 +41,7 @@ public class SignUpVersion2 {
     }
 
     @Test
-    public void user_ShouldNotBe_SignedUp_WithEmptyZipCode(){
+    public void user_ShouldNotBe_SignedUp_WithEmptyZipCode() {
         driver.findElement(By.cssSelector("input[value='Continue']")).click();
         WebElement errorMessage = driver.findElement(By.className("error_message"));
         assertTrue(errorMessage.isDisplayed(), "Error message is not displayed in case of empty zip code");
@@ -49,7 +49,7 @@ public class SignUpVersion2 {
     }
 
     @Test
-    public void userValidInfoSignUp(){
+    public void userValidInfoSignUp() {
         driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=67890");
         driver.findElement(By.cssSelector("input[name='first_name']")).sendKeys("Mark");
         driver.findElement(By.cssSelector("input[name='last_name']")).sendKeys("Tester");
@@ -58,15 +58,15 @@ public class SignUpVersion2 {
         driver.findElement(By.cssSelector("input[name='password2']")).sendKeys("0234adam_smith");
         driver.findElement(By.cssSelector("input[value='Register")).click();
         assertTrue(driver.findElement(By.className("confirmation_message")).isDisplayed(),
-        "The message\"Account is created\" is displayed");
+                "The message\"Account is created\" is displayed");
     }
 
     @Test
-    public void userInvalidInfo(){
+    public void userInvalidInfo() {
         driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=67890");
         driver.findElement(By.cssSelector("input[value='Register")).click();
         WebElement errorMsg = driver.findElement(By.className("error_message"));
-        assertTrue(errorMsg.isDisplayed(),"Error message is not displayed in case of registration with empty user info");
+        assertTrue(errorMsg.isDisplayed(), "Error message is not displayed in case of registration with empty user info");
         assertEquals(errorMsg.getText(), USER_INFO_ERROR, "Error message should be correct");
 
     }
@@ -76,9 +76,8 @@ public class SignUpVersion2 {
 //    Password	1111
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
-
 
 }
