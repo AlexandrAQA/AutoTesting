0dfec92e1643c7b36cb869c7be069a71f8849649
AlexPanaev <panaev.as@gmail.com>
2023-01-03 18:40:21 +0400

    master:pageObject:@FindBy(),WAIT, FluentWait<>(driver)

--- src/main/java/aqa/pageObjectPattern/pages/BasePage.java
+++ src/main/java/aqa/pageObjectPattern/pages/BasePage.java
@@ -1,13 +1,20 @@
 package aqa.pageObjectPattern.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BasePage {
 
     WebDriver driver;
     public static final String BASE_URL = "https://www.saucedemo.com/";
+    WebDriverWait wait;
 
     public BasePage(WebDriver driver) {
+
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
     }
 }
--- src/main/java/aqa/pageObjectPattern/pages/LoginPage.java
+++ src/main/java/aqa/pageObjectPattern/pages/LoginPage.java
@@ -3,24 +3,32 @@ package aqa.pageObjectPattern.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class LoginPage extends BasePage {
 
     @FindBy(id = "user-name")
+    @CacheLookup //searching 1 time then we get it from cache
     private WebElement userNameInput;
+
     @FindBy(id = "password")
+    @CacheLookup
     private WebElement passwordInput;
+
     @FindBy(id = "login-button")
+    @CacheLookup
     private WebElement loginButton;
+
     @FindBy(css = "h3[data-test=error]")
     private WebElement error;
 
-//    private final By USER_NAME_INPUT = (By.id("user-name"));
-//    private final By PASSWORD_INPUT = (By.id("password"));
-//    private final By LOGIN_BUTTON = (By.id("login-button"));
-//    private final By ERROR = (By.cssSelector("h3[data-test=error]"));
 
     public LoginPage(WebDriver driver) {
         super(driver);
@@ -29,6 +37,11 @@ public class LoginPage extends BasePage {
 
     public void openPage() {
         driver.get(BASE_URL);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login-button")));
+        Wait<WebDriver> fluentWait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(30))
+                .ignoring(NoSuchMethodException.class);
     }
 
     public void login(String user, String password) {
